76a2e52352d3b7acd25897523e09dca2e7369e8e
onionMit <minhho.technology@gmail.com>
2020-04-06 09:05:00 -0700

    handling time

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-var time = 121;
-function startTime(){
+var time = 5;
+function timeshow(){
     time--;
     ss = time % 60;
     mm = Math.floor(time/60);
     document.getElementById("timer").innerHTML= mm+':'+ ss;
+    if(mm == 0&&ss== 0){
+        alert("Hết giờ");
+        stopTime();
+    }
 }
-function setIntervalTime(){
-    setInterval(this.startTime,1000);
+function startTime(){
+     timeInterval = setInterval(this.timeshow,1000);
+}
+function stopTime(){
+    clearInterval(timeInterval);
 }
 
 
